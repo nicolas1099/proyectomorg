6a257caecc552e6119b1a7a1b480b32523fca7ec
nicolas1099 <nikorivas10@gmail.com>
2021-03-09 14:19:47 +0100

    martes2

---
+++ proyecto/database/migrations/2021_03_09_124615_create_usuarios_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->unsignedinteger('id')->primary();
+            $table->string('pasword',255);
+            $table->string('nombre',255);
+            $table->string('apellido',255);
+            $table->string('mail',255);
+            $table->biginteger('perfilid')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+}
