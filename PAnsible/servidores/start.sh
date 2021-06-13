9264baaf1f0c345b22a286fef84992dd4a544c9a
Johan César Robles Cuéllar <jcrcink@gmail.com>
2021-06-13 08:51:19 +0200

    proyecto ansible

---
+++ PAnsible/servidores/start.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+
+newUser(){
+    # ---------------- creación de usuario 
+    echo "MAQ2-->usuarioBD-->${USUARIO}" > /root/datos.txt
+    useradd -rm -d /home/"${USUARIO}" -s /bin/bash "${USUARIO}" 
+    echo "root:${PASSWD}" | chpasswd
+    echo "${USUARIO}:${PASSWD}" | chpasswd
+}
+
+config_Sudoers(){
+    echo "${USUARIO} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+}
+
+config_ssh(){
+    sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+    sed -i 's/#Port 22/Port 22/' /etc/ssh/sshd_config
+    if [ ! -d /home/${USUARIO}/.ssh ]
+    then
+        mkdir /home/${USUARIO}/.ssh
+        cat /root/id_rsa.pub >> /home/${USUARIO}/.ssh/authorized_keys
+    fi
+    /etc/init.d/ssh start
+}
+
+
+
+
+main(){
+    newUser
+    config_Sudoers
+    config_ssh
+}
+
+main
+tail -f /dev/null
+
